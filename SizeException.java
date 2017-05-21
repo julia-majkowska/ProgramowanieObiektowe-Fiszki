3ea1f58fbfaf8101355a8ba8eff6aa533238aa8b
agaga23 <agaga.dudek@gmail.com>
2017-05-21 15:26:33 +0200

    Wybór/zapis użytkowników
    Wybór użytkownika z konsoli, zapisywanie listy użytkowników do pliku

---
+++ SizeException.java
@@ -0,0 +1,6 @@
+public class SizeException extends Exception{
+	public SizeException() { super(); }
+	public SizeException(String m) { super(m); }
+	public SizeException(String m, Throwable c) { super(m, c); }
+	public SizeException(Throwable c) { super(c); }
+}
