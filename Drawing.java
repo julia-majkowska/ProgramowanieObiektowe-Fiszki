2914128be52a9cb5815883b32a3bb2cd5e392a00
agaga23 <agaga.dudek@gmail.com>
2017-06-22 16:11:05 +0200

    dra

---
+++ Drawing.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+public abstract class Drawing{
+	Date today;
+	NBase nfb;
+	RBase rfb;
+	User mr;
+	
+	HashMap<String,Integer> topicsSize;
+
+	abstract void countTopicsSize();
+	abstract void takeTopic(String t);
+	abstract RFiszka takeCard(String topic, int nr);
+	int[] chooseCards(String topic, int howMany){
+		Random generator = new Random();
+		
+		int range = topicsSize.get(topic) - 1;
+		int[] chosenCards = new int[howMany];
+		for ( int i = howMany - 1; i >= 0; i -- ){
+			int margin = range - i; //taki mam zapas
+			int rand = generator.nextInt(margin);
+			chosenCards[i] = range - rand;
+			range = chosenCards[i] - 1;
+		}
+		return chosenCards;
+	}
+	Vector<RFiszka> selectCards(String topic, int howMany){
+		int[] numbers = chooseCards(topic, howMany);
+		
+		takeTopic(topic);
+		Vector<RFiszka> selectedCards = new Vector<RFiszka>();
+		for ( int i = 0; i < numbers.length; i ++ ){
+			selectedCards.add(takeCard(topic,numbers[i]));
+		}
+		return selectedCards;
+	}
+	
+	Drawing(User imr){
+		nfb = imr.nf;
+		mr = imr;
+		rfb = new RBase(mr.id, nfb, today);
+		countTopicsSize();
+	}
+}
---
+++ Learning.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+public class Learning extends Drawing{
+	HashMap<String,Vector<Fiszka> > goodF;
+	
+	RFiszka takeCard(String topic, int nr){
+		return new RFiszka(goodF.get(topic).get(nr), today);
+	}
+// 			int newVal = topicsSize.get(topic)-1;
+// 			if ( newVal == 0 ) topicsSize.remove(topic);
+// 			else topicsSize.put(topic,topicsSize.get(topic)-1); i zmienic rf
+	void takeTopic(String topic){
+		if ( goodF.get(topic) != null ) return;
+	 
+		Vector<Fiszka> topicV = new Vector<Fiszka>();
+		for ( int i = 0; i < nfb.topic_sort.get(topic).size(); i ++ ){
+			Fiszka toCheck = nfb.topic_sort.get(topic).get(i);
+			if ( !rfb.ids.contains(toCheck.id) ) topicV.add(toCheck);
+		}
+		goodF.put(topic, topicV);
+	}
+	void countTopicsSize(){
+		topicsSize = new HashMap<String,Integer>();
+		for (String topic: nfb.topic_sort.keySet() ){
+			int size = nfb.topic_sort.get(topic).size();
+			if (rfb.topic_sort.get(topic) != null ) 
+				size -= rfb.topic_sort.get(topic).size();
+			if ( size > 0 ) topicsSize.put(topic, size);
+		}
+	}
+	Learning(User imr){
+		super(imr);
+		countTopicsSize();
+	}
+}
