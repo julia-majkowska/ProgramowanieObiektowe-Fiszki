bb90d8c88140859b293a88b0b680ea6a57e8984c
Bartek Dudek <bardek.dudek@gmail.com>
2017-06-26 21:31:19 +0200

    Aga

--- FieldData.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-import java.io.*;
-
-public class FieldData implements Serializable
-{
-    public String typ = "TEKST";
-    public String nazwa = "nazwa";
-}
--- UserList2.java
+++ UserList2.java
@@ -1,85 +1,46 @@
 import java.io.*;
-import java.util.Scanner;
-class User implements Serializable{
-	private static final long serialVersionUID = 95847483;
-	String login;
-	int id;
-// 	Base rfBase = new Base(); //base with fiszkas to repeat
-  
-	public String toString(){
-		return login;
-	}
-	public void readObject(ObjectInputStream stream) throws ClassNotFoundException, IOException{
-		login = (String) stream.readObject();
-		id = stream.readInt();
-// 		rfBase = (Base) stream.readObject();
-	}
-	public void writeObject(ObjectOutputStream stream) throws IOException{
-		stream.writeObject(login);
-		stream.writeObject(id);
-// 		stream.writeObject(rfBase);
-		//potem base
-	}
-	User(String l, int i){
-		login = l;
-		id = i;
-	}
-}
+import java.util.*;
 public class UserList2 implements Serializable{
 	private static final long serialVersionUID = 95847483;
 
 	int size = 0, nextid = 0;
-	UserLogin[] uLogins = new UserLogin[10];
-	
-	void addUser(UserLogin newLogin) throws SizeException, StopException{
-		if ( size == 10 ) throw new SizeException("Nie możesz dodać 11. użytkownika, to za dużo.");
-		for ( int i = 0; i < size; i ++ )
-			if ( uLogins[i].login.equals(newLogin.login) ) throw new StopException("Użytkownik o takim loginie już istnieje. Wybierz inny login.");
-		newLogin.id = nextid ++;
-		uLogins[size++] = newLogin;
-	}
-	void deleteUser(int nr){
-		for(int i = nr + 1; i < size; i ++ ) uLogins[i - 1] = uLogins[i];
-		size --;
-	}
-	void deleteUser(UserLogin toDeleteLogin) throws StopException{
-		for ( int i = 0; i < size; i ++ ){
-			if (toDeleteLogin.login == uLogins[i].login){
-				if ( toDeleteLogin.password == uLogins[i].password ){
-					for ( int j = i + 1; j < size; j ++ ) uLogins[i - 1] = uLogins[i];
-					size --;
-					return;
-				}
-				else throw new StopException("Podałeś złe hasło");	   
+	LinkedList<UserLogin> uLogins;
+
+	boolean addUser(UserLogin newLogin){
+		for ( UserLogin ul: uLogins ){
+			if ( ul.login.equals(newLogin.login) ) {
+				System.out.println("Użytkownik o takim loginie już istnieje. Wybierz inny login.");
+				return false;
 			}
 		}
-		throw new StopException("Użytkownik o takim loginie nie istnieje");
+		newLogin.id = nextid ++;
+		uLogins.add(newLogin);
+		try{
+			writeUserList2();
+		}catch (Exception e){
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
-	User chooseUser(UserLogin typedLogin) throws StopException{
-		for ( int i = 0; i < size; i ++ ){
-			if ( uLogins[i].login.equals(typedLogin.login) ){
-				if ( uLogins[i].password.equals(typedLogin.password) == false )
-					throw new StopException("Podałeś złe hasło. Spróbuj jeszcze raz.");
-				if (uLogins[i].toDelete == true ) {
-					deleteUser(i);
+	User chooseUser(UserLogin typedLogin){
+		for ( UserLogin ul: uLogins ){
+			if ( ul.login.equals(typedLogin.login) ){
+				if ( ul.password.equals(typedLogin.password) == false ){
+					System.out.println("Podałeś złe hasło. Spróbuj jeszcze raz.");
 					return null;
 				}
-				else{
-					User user = new User(uLogins[i].login, uLogins[i].id);
-					return user;
-				}
+				User user = new User(ul.login, ul.id);
+				return user;
 			}
 		}
+		System.out.println("Użytkownik o takim loginie nie istnieje.");
 		return null;
 	}
 	UserList2(){
 		try{
 			readUserList2();
-		}catch(IOException e) {
-			e.printStackTrace();
-			return;
-		}catch(ClassNotFoundException e) {
-			System.out.println("UserList2 class not found");
+		}catch(Exception e) {
 			e.printStackTrace();
 			return;
 		}
@@ -91,11 +52,12 @@ public class UserList2 implements Serializable{
 			ObjectInputStream in = new ObjectInputStream(fileIn);
 			size = in.readInt();
 			nextid = in.readInt();		
-			for ( int i = 0; i < size; i ++ ) uLogins[i] = (UserLogin) in.readObject();
+			uLogins = (LinkedList<UserLogin>)in.readObject();
 
 			in.close();
 			fileIn.close();
 		}
+		else uLogins = new LinkedList<UserLogin>();
 	}
 	void writeUserList2() throws ClassNotFoundException, IOException{
 		File f = new File("Users.txt");
@@ -103,7 +65,7 @@ public class UserList2 implements Serializable{
 		ObjectOutputStream out = new ObjectOutputStream(fileOut);
 		out.writeInt(size);
 		out.writeInt(nextid);
-		for ( int i = 0; i < size; i ++ ) out.writeObject(uLogins[i]);
+		out.writeObject(uLogins);
 		out.close();
 		fileOut.close();
 	}
